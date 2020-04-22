364af583cd9666313b81395a8368bd70e1058185
Artur <artur.zich@hs-furtwangen.de>
2020-04-22 08:36:34 +0200

    Update

---
+++ First_Fudge/Main.js
@@ -0,0 +1,11 @@
+"use strict";
+var First_Fudge;
+(function (First_Fudge) {
+    var fi = FudgeCore;
+    let node = new fi.Node("Test_Node");
+    console.log(node);
+    // Viewport
+    // Camera
+    // Node
+})(First_Fudge || (First_Fudge = {}));
+//# sourceMappingURL=Main.js.map
---
+++ First_Fudge/Main.ts
@@ -0,0 +1,13 @@
+namespace First_Fudge {
+
+    import fi = FudgeCore;
+
+    let node: fi.Node = new fi.Node("Test_Node");
+    console.log(node);
+
+    // Viewport
+
+    // Camera
+
+    // Node
+}
